ca6f3548d4f18f7c6e10e9074062ff5eff5436bf
Mark <mark.n.bacon.jr@gmail.com>
2022-04-04 20:09:53 -0400

    commit tree 948d508a4b662f65df5c7a98c5d9a64b69371250

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props)
+  if (!props.isPublished) {
+    return null
+  } else {
+
+    return (
+      <div>
+
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} Minute to Read</p>
+      </div>
+    )
+
+  }
 }
 
 export default BlogContent;
